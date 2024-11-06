c3f6c2e09fd8275b369e218912e809abb24f7ad5
Serghei Catraniuc <catraniuc.serghei@gmail.com>
2024-11-06 14:30:25 +1100

    Initial Commit

---
+++ CryptoFeed/CryptoFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoFeedApp.swift
+//  CryptoFeed
+//
+//  Created by Serghei on 06.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CryptoFeedTests/CryptoFeedTests.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoFeedTests.swift
+//  CryptoFeedTests
+//
+//  Created by Serghei on 06.11.2024.
+//
+
+import Testing
+@testable import CryptoFeed
+
+struct CryptoFeedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
